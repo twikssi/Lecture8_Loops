da1cb2c4b2430d32c2424e9ed789aeb23cb1b9a8
twikssi <limpbiski15@gmail.com>
2020-09-10 18:18:48 +0300

    created class ForOddChecker

---
+++ src/main/java/by/andrey/twikssi/exer1tableoddnumbers/service/ForOddChecker.java
@@ -0,0 +1,13 @@
+package by.andrey.twikssi.exer1tableoddnumbers.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForOddChecker {
+    public static Logger logger = LoggerFactory.getLogger(ForOddChecker.class);
+
+    private ForOddChecker() {
+    }
+
+
+}
